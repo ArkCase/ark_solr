12f867c3e61af0f3e793a58fac9f62539f0d3c5b
Kerati Naveen <naveenk@Keratis-MacBook-Pro.local>
2021-07-21 16:33:01 +0530

    changes added

--- get-artifacts.sh
+++ get-artifacts.sh
@@ -3,8 +3,7 @@ here=$(realpath "$0")
 here=$(dirname "$here")
 cd "$here"
 SOLR_VERSION="7.7.2"
-cloud_config_version="2021.03"
 rm -rf artifacts
 mkdir artifacts
-echo "Downloading  solr version $cloud_config_version"
+echo "Downloading  solr version $SOLR_VERSION"
 aws s3 cp "s3://arkcase-container-artifacts/ark_solr/artifacts/solr-${SOLR_VERSION}.tgz" artifacts/
