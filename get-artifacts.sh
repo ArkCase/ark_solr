b9b61b4a1944f3f4553b84d6e2318a550f504eea
Jon Holman <jon.holman@armedia.com>
2021-07-23 10:35:22 -0400

    Update get-artifacts.sh

--- get-artifacts.sh
+++ get-artifacts.sh
@@ -5,5 +5,5 @@ cd "$here"
 SOLR_VERSION="7.7.2"
 rm -rf artifacts
 mkdir artifacts
-echo "Downloading  solr version $SOLR_VERSION"
+echo "Downloading solr version $SOLR_VERSION"
 aws s3 cp "s3://arkcase-container-artifacts/ark_solr/artifacts/solr-${SOLR_VERSION}.tgz" artifacts/
